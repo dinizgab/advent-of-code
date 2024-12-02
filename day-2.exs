40ec0ee97f8fc94f772b03ffe56f97ff9b50f252
Gabriel Diniz <gabriel.diniz@inda.band>
2024-12-02 19:06:22 -0300

    feat: starting day2-part2

--- day-2.exs
+++ day-2.exs
@@ -68,8 +68,35 @@ defmodule Day2 do
 end
 
 defmodule Day2Part2 do
+  def ascending(list) do
+    Enum.chunk_every(list, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a < b end)
+  end
+
+  def descending(list) do
+    Enum.chunk_every(list, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a > b end)
+  end
+
+  def removedOne(list) do
+    for x <- list, do: List.delete(list, x)
+  end
+
   def run do
+    input_path = "inputs/day-2.txt"
+
+    input =
+      File.stream!(input_path)
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.split(&1, " "))
+      |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+
+    IO.puts(ascending([1, 2, 3, 4, 5]))
+    IO.puts(ascending([1, 2, 2, 4, 5]))
+    IO.inspect(removedOne([1, 2, 3, 4, 5]))
+    IO.puts(ascending([1]))
   end
 end
 
 Day2.run()
+Day2Part2.run()
