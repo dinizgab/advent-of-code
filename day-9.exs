64812c2e2ac5ae8b63fc161f06cabf562e852601
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-12-12 13:08:40 -0300

    feat: fixing day 9

--- day-9.exs
+++ day-9.exs
@@ -1,13 +1,4 @@
 defmodule Day9 do
-  def swap(disk, curr_index, last_index) do
-    e1 = Enum.at(disk, curr_index)
-    e2 = Enum.at(disk, last_index)
-
-    disk
-    |> List.replace_at(curr_index, e2)
-    |> List.replace_at(last_index, e1)
-  end
-
   def format_disk(disk) do
     case disk do
       [] ->
@@ -26,11 +17,15 @@ defmodule Day9 do
             |> Enum.reverse()
             |> Enum.find_index(fn v -> v != "." end)
 
-          last_valid_element = Enum.at(tail, last_valid_index)
-          IO.inspect(last_valid_element)
-          {_, popped_list} = List.pop_at(tail, -1)
+          if last_valid_index == nil do
+            [head] ++ format_disk(tail)
+          else
+            index_offset = length(tail) - last_valid_index - 1
+            last_valid_element = Enum.at(tail, index_offset)
+            {_, popped_list} = List.pop_at(tail, index_offset)
 
-          [last_valid_element] ++ format_disk(popped_list) ++ [head]
+            [last_valid_element] ++ format_disk(popped_list) ++ [head]
+          end
         else
           [head] ++ format_disk(tail)
         end
@@ -38,29 +33,40 @@ defmodule Day9 do
   end
 
   def run do
-    input = "2333133121414131402"
+    input =
+      File.read!("inputs/day-9.txt")
+      |> String.graphemes()
 
     disk_value =
-      String.graphemes(input)
+      input
+      |> List.delete_at(length(input) - 1)
       |> Enum.with_index()
-      |> Enum.reduce("", fn {v, i}, acc ->
+      |> Enum.reduce([], fn {v, i}, acc ->
         if rem(i, 2) == 0 do
-          acc <> String.duplicate(Integer.to_string(div(i, 2)), elem(Integer.parse(v), 0))
+          acc ++ [String.duplicate(Integer.to_string(div(i, 2)), elem(Integer.parse(v), 0))]
         else
-          acc <> String.duplicate(".", elem(Integer.parse(v), 0))
+          acc ++ [String.duplicate(".", elem(Integer.parse(v), 0))]
         end
       end)
 
-    IO.inspect(disk_value)
+    IO.inspect(disk_value, limit: :infinity)
 
     # Search for first valid index from left to right
     # Search for first valid index from right to left
     # Swap them
-    formated_disk_value =
-      String.graphemes(disk_value)
-      |> format_disk()
+    # Problem lies here, i can't send String.graphemes, because it splits the string into individual characters
+    checksum_value =
+       format_disk()
+      |> Enum.with_index()
+      |> Enum.reduce(0, fn {v, i}, acc ->
+        if v != "." do
+          acc + elem(Integer.parse(v), 0) * i
+        else
+          acc
+        end
+      end)
 
-    IO.inspect(formated_disk_value)
+    IO.inspect(checksum_value)
   end
 end
 
